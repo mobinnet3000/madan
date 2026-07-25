402e3f1126eb052c4083bf686a2c9157c9db477c
mobin bastani <mobinnet3000@gmail.com>
2026-07-25 18:37:57 +0330

    رفع ۴۰۴: انتقال import گزارش‌ها به داخل تابع، try-catch خطا، ساده‌سازی منطق view

--- machines/views.py
+++ machines/views.py
@@ -120,92 +120,73 @@ def report_ranges_view(request):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def performance_report_view(request):
-    factory_id = request.query_params.get('factory_id')
+    # Bypass everything - just generate and return
+    from machines.reports import generate_performance_report as gen_report
     range_key = request.query_params.get('range', '30days')
-    date_from = request.query_params.get('date_from')
-    date_to = request.query_params.get('date_to')
     fmt = request.query_params.get('format', 'pdf').lower()
+    factory_id = request.query_params.get('factory_id')
 
     factory = get_user_factory(request.user)
     if factory and not factory_id:
         factory_id = factory.id
-
     if factory_id:
         try:
             fac_obj = Factory.objects.get(id=factory_id)
         except Factory.DoesNotExist:
             raise Http404
-    else:
-        fac_obj = None
 
+    from datetime import datetime
     date_from_o = None
     date_to_o = None
-    if date_from:
-        try:
-            from datetime import datetime
-            date_from_o = datetime.strptime(date_from, '%Y-%m-%d').date()
-        except ValueError:
-            pass
-    if date_to:
-        try:
-            from datetime import datetime
-            date_to_o = datetime.strptime(date_to, '%Y-%m-%d').date()
-        except ValueError:
-            pass
+    df = request.query_params.get('date_from')
+    dt = request.query_params.get('date_to')
+    if df:
+        try: date_from_o = datetime.strptime(df, '%Y-%m-%d').date()
+        except: pass
+    if dt:
+        try: date_to_o = datetime.strptime(dt, '%Y-%m-%d').date()
+        except: pass
 
-    if fmt == 'excel':
-        buf, ext = generate_performance_report(factory_id, range_key, date_from_o, date_to_o, fmt='excel')
-        filename = f'performance_report_{factory_id or "all"}_{range_key}.xlsx'
-        return FileResponse(buf, as_attachment=True, filename=filename,
-                            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    else:
-        buf, ext = generate_performance_report(factory_id, range_key, date_from_o, date_to_o, fmt='pdf')
-        filename = f'performance_report_{factory_id or "all"}_{range_key}.pdf'
-        return FileResponse(buf, as_attachment=True, filename=filename,
-                            content_type='application/pdf')
+    try:
+        buf, ext = gen_report(factory_id, range_key, date_from_o, date_to_o, fmt=fmt)
+    except Exception as e:
+        return Response({'error': str(e)}, status=500)
+
+    ctype = 'application/pdf' if fmt == 'pdf' else 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    fname = f'report_{factory_id or "all"}_{range_key}.{fmt}'
+    return FileResponse(buf, as_attachment=True, filename=fname, content_type=ctype)
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def analysis_report_view(request):
-    factory_id = request.query_params.get('factory_id')
+    from machines.reports import generate_analysis_report as gen_report
     range_key = request.query_params.get('range', '30days')
-    date_from = request.query_params.get('date_from')
-    date_to = request.query_params.get('date_to')
     fmt = request.query_params.get('format', 'pdf').lower()
+    factory_id = request.query_params.get('factory_id')
 
     factory = get_user_factory(request.user)
     if factory and not factory_id:
         factory_id = factory.id
-
     if factory_id:
         try:
             fac_obj = Factory.objects.get(id=factory_id)
         except Factory.DoesNotExist:
             raise Http404
 
+    from datetime import datetime
     date_from_o = None
     date_to_o = None
-    if date_from:
-        try:
-            from datetime import datetime
-            date_from_o = datetime.strptime(date_from, '%Y-%m-%d').date()
-        except ValueError:
-            pass
-    if date_to:
-        try:
-            from datetime import datetime
-            date_to_o = datetime.strptime(date_to, '%Y-%m-%d').date()
-        except ValueError:
-            pass
+    df = request.query_params.get('date_from')
+    dt = request.query_params.get('date_to')
+    if df:
+        try: date_from_o = datetime.strptime(df, '%Y-%m-%d').date()
+        except: pass
+    if dt:
+        try: date_to_o = datetime.strptime(dt, '%Y-%m-%d').date()
+        except: pass
 
-    if fmt == 'excel':
-        buf, ext = generate_analysis_report(factory_id, range_key, date_from_o, date_to_o, fmt='excel')
-        filename = f'analysis_report_{factory_id or "all"}_{range_key}.xlsx'
-        return FileResponse(buf, as_attachment=True, filename=filename,
-                            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    else:
-        buf, ext = generate_analysis_report(factory_id, range_key, date_from_o, date_to_o, fmt='pdf')
-        filename = f'analysis_report_{factory_id or "all"}_{range_key}.pdf'
-        return FileResponse(buf, as_attachment=True, filename=filename,
-                            content_type='application/pdf')
+    buf, ext = gen_report(factory_id, range_key, date_from_o, date_to_o, fmt=fmt)
+    ctype = 'application/pdf' if fmt == 'pdf' else 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    fname = f'report_{factory_id or "all"}_{range_key}.{fmt}'
+    return FileResponse(buf, as_attachment=True, filename=fname, content_type=ctype)
